fbf3f1f4cddcf02520c973f857047b2462288dd3
Matt Janda <matt.janda.kingston@gmail.com>
2020-06-14 08:42:40 +0100

    feat: Add assembly logger

---
+++ src/Janda.Runtime.Assembly/AssemblyLoggerExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Janda.Runtime
+{
+    public static class AssemblyLoggerExtensions
+    {
+        public static IServiceCollection AddAssemblyLogger(this IServiceCollection services)
+        {
+            return services.AddTransient<IAssemblyLogger, AssemblyLogger>();
+        }
+    }
+}
